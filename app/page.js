22af45599f6160ba79e2a199e2d1c25c45c3eb9d
mahar noor <maharnoor380@gmail.com>
2026-03-07 13:51:36 +0500

    feat: Implement initial homepage with basic UI, add README, and remove old components config backup.

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -6,7 +6,7 @@ export default function Home() {
       <div>
         MindField
         <br />
-        
+        <Button>hello</Button>
       </div>
-  );
+  ) ;
 }
